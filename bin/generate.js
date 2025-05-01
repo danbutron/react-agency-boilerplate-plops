51a83839fd0589e92212689f74298cb54c484b4b
Raul Daniel Butron Otero <daniel.butron@anseris.es>
2025-05-01 08:11:23 -0600

    fix: convert to commonjs

--- bin/generate.js
+++ bin/generate.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { run } from 'plop';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const path = require('path');
+const { run } = require('plop');
 
 const plopfilePath = path.resolve(__dirname, '../plopfile.js');
 
-run({
-    cwd: process.cwd(),
+run(undefined, {
     configPath: plopfilePath,
 });
